874c4907011b5235bdd9c45fdcf86b65a4bba9f7
Todd Burry <todd@vanillaforums.com>
2017-01-11 18:16:24 -0500

    Add the Query class

---
+++ src/Query.php
@@ -0,0 +1,257 @@
+<?php
+/**
+ * @author Todd Burry <todd@vanillaforums.com>
+ * @copyright 2009-2017 Vanilla Forums Inc.
+ * @license MIT
+ */
+
+namespace Garden\Db;
+
+/**
+ * An object representation of a SELECT statement.
+ */
+class Query {
+    /**
+     * @var string
+     */
+    private $from;
+
+    /**
+     * @var array The where clause.
+     */
+    private $where;
+
+    /**
+     * @var array A pointer to the current where clause.
+     */
+    private $currentWhere;
+
+    /**
+     * @var array Pointers to nested where brackets.
+     */
+    private $whereStack;
+
+    /**
+     * @var int
+     */
+    private $limit;
+
+    /**
+     * @var int
+     */
+    private $offset;
+
+    /**
+     * Instantiate a new instance of the {@link Query} class.
+     */
+    public function __construct() {
+        $this->where = [];
+        $this->currentWhere = &$this->where;
+        $this->whereStack = [];
+    }
+
+    /**
+     * Begin an AND bracket group in the WHERE clause.
+     *
+     * @return $this
+     * @see Query::end()
+     */
+    public function beginAnd() {
+        $this->beginBracket(Db::OP_AND);
+        return $this;
+    }
+
+    /**
+     * Begin an OR bracket group in the WHERE clause.
+     *
+     * @return $this
+     * @see Query::end()
+     */
+    public function beginOr() {
+        $this->beginBracket(Db::OP_OR);
+        return $this;
+    }
+
+    /**
+     * Begin a bracket group in the WHERE clause.
+     *
+     * @param string $op One of the **DB::OP_*** constants.
+     * @return $this
+     */
+    private function beginBracket($op) {
+        $this->currentWhere[] = [$op => []];
+        $this->whereStack[] = &$this->currentWhere;
+        end($this->currentWhere);
+        $this->currentWhere = &$this->currentWhere[key($this->currentWhere)][$op];
+
+        return $this;
+    }
+
+    /**
+     * End a bracket group.
+     *
+     * @return $this
+     * @see Query::beginAnd(), Query::beginOr()
+     */
+    public function end() {
+        // First unset the reference so it doesn't overwrite the original where clause.
+        unset($this->currentWhere);
+
+        // Move the pointer in the where stack back one level.
+        if (empty($this->whereStack)) {
+            trigger_error("Call to Query->end() without a corresponding call to Query->begin*().", E_USER_NOTICE);
+            $this->currentWhere = &$this->where;
+        } else {
+            $key = key(end($this->whereStack));
+            $this->currentWhere = &$this->whereStack[$key];
+            unset($this->whereStack[$key]);
+
+        }
+        return $this;
+    }
+
+    /**
+     * Add a WHERE condition to the query.
+     *
+     * A basic call to this method specifies a column name and a value.
+     *
+     * ```php
+     * $query->where('id', 123);
+     * ```
+     *
+     * If you specify just a value then the query will perform an equality match. If you want to specify a different
+     * operator then you can pass in an array for {@link $value} with the key being the operator and the value being the
+     * value to filter on.
+     *
+     * ```php
+     * $query->where('count', [Db::GT => 100]);
+     * ```
+     *
+     * @param string $column Either the name of the column or an array representing several query operators.
+     * @param mixed $value The value to match to the column. If using the array form of {@link $column} then this parameter
+     * can be omitted.
+     * @return $this
+     */
+    public function addWhere($column, $value) {
+        if (array_key_exists($column, $this->currentWhere)) {
+            if (!is_array($this->currentWhere[$column])) {
+                // This is a basic equality statement.
+                $this->currentWhere[$column] = [Db::OP_EQ => $this->currentWhere[$column]];
+            } elseif (array_key_exists(0, $this->currentWhere[$column])) {
+                // This is a basic IN clause.
+                $this->currentWhere[$column] = [Db::OP_IN => $this->currentWhere[$column]];
+            }
+
+            // Massage the value for proper syntax.
+            if (!is_array($value)) {
+                $value = [Db::OP_EQ => $value];
+            } elseif (array_key_exists(0, $value)) {
+                $value = [Db::OP_IN => $value];
+            }
+
+            $this->currentWhere[$column] = array_merge($this->currentWhere[$column], $value);
+        } else {
+            $this->currentWhere[$column] = $value;
+        }
+        return $this;
+    }
+
+    /**
+     * Add an array of where statements.
+     *
+     * This method takes an array where the keys represent column names and the values represent filter values.
+     *
+     * ```php
+     * $query->where([
+     *     'parentID' => 123,
+     *     'count' => [Db::GT => 0]
+     * ]);
+     * ```
+     *
+     * @param array $where The array of where statements to add to the query.
+     * @return $this
+     * @see Query::addWhere()
+     */
+    public function addWheres(array $where) {
+        foreach ($where as $column => $value) {
+            $this->addWhere($column, $value);
+        }
+        return $this;
+    }
+
+    /**
+     * Return an array representation of this query.
+     *
+     * @return array Returns an array with keys representing the query parts.
+     */
+    public function toArray() {
+        $r = [
+            'from' => $this->from,
+            'where' => $this->where,
+            'limit' => $this->limit,
+            'offset' => $this->offset
+        ];
+
+        return $r;
+    }
+
+    /**
+     * Get the from.
+     *
+     * @return string Returns the from.
+     */
+    public function getFrom() {
+        return $this->from;
+    }
+
+    /**
+     * Set the from.
+     *
+     * @param string $from
+     * @return $this
+     */
+    public function setFrom($from) {
+        $this->from = $from;
+        return $this;
+    }
+
+    /**
+     * Get the limit.
+     *
+     * @return int Returns the limit.
+     */
+    public function getLimit() {
+        return $this->limit;
+    }
+
+    /**
+     * Set the limit.
+     *
+     * @param int $limit
+     * @return $this
+     */
+    public function setLimit($limit) {
+        $this->limit = $limit;
+        return $this;
+    }
+
+    /**
+     * Get the offset.
+     *
+     * @return int Returns the offset.
+     */
+    public function getOffset() {
+        return $this->offset;
+    }
+
+    /**
+     * Set the offset.
+     *
+     * @param int $offset
+     * @return $this
+     */
+    public function setOffset($offset) {
+        $this->offset = $offset;
+        return $this;
+    }
+}
---
+++ tests/QueryTest.php
@@ -0,0 +1,141 @@
+<?php
+/**
+ * @author Todd Burry <todd@vanillaforums.com>
+ * @copyright 2009-2017 Vanilla Forums Inc.
+ * @license Proprietary
+ */
+
+namespace Garden\Tests\Db;
+
+use Garden\Db\Db;
+use Garden\Db\Query;
+
+/**
+ * Test the {@link Query} class.
+ */
+class QueryTest extends \PHPUnit_Framework_TestCase {
+    /**
+     * Test a basic bracketed expression.
+     */
+    public function testBasicBrackets() {
+        $qry = new Query();
+
+        $qry->beginAnd()
+            ->addWhere('foo', 'bar')
+            ->addWhere('bar', 'baz')
+            ->end();
+
+        $where = $qry->toArray()['where'];
+        $this->assertEquals(
+            [[Db::OP_AND => ['foo' => 'bar', 'bar' => 'baz']]],
+            $where
+        );
+    }
+
+    /**
+     * {link Query::beginAnd()} and {link Query::beginOr()} should be similar.
+     */
+    public function testBeginOr() {
+        $qry = new Query();
+
+        $qry->beginOr()
+            ->addWhere('foo', 'bar')
+            ->end();
+
+        $this->assertEquals(
+            [[Db::OP_OR => ['foo' => 'bar']]],
+            $qry->toArray()['where']
+        );
+    }
+
+    /**
+     * You should be able to add where expressions after calling {@link Query::end()}.
+     */
+    public function testWhereAfterEnd() {
+        $qry = new Query();
+
+        $qry->beginAnd()
+            ->addWhere('foo', 'bar')
+            ->end()
+            ->addWhere('bar', 'baz');
+
+        $this->assertEquals(
+            [[Db::OP_AND => ['foo' => 'bar']], 'bar' => 'baz'],
+            $qry->toArray()['where']
+        );
+    }
+
+    /**
+     * You should be able to nest where groups.
+     */
+    public function testNestedWhere() {
+        $qry = new Query();
+
+        $qry->beginAnd()
+            ->addWhere('foo', 'bar')
+            ->beginOr()
+            ->addWhere('bar', 'baz')
+            ->end()
+            ->addWhere('baz', 'bam')
+            ->end();
+
+        $this->assertEquals(
+            [[Db::OP_AND => ['foo' => 'bar', [Db::OP_OR => ['bar' => 'baz']]]], 'baz' => 'bam'],
+            $qry->toArray()['where']
+        );
+    }
+
+    /**
+     * You should be able to pass multiple where values as an array to {@link Query::where()}.
+     */
+    public function testWhereArray() {
+        $qry = new Query();
+
+        $qry->addWheres(['foo' => 'bar', 'bar' => 'baz']);
+
+        $this->assertEquals(
+            ['foo' => 'bar', 'bar' => 'baz'],
+            $qry->toArray()['where']
+        );
+    }
+
+    /**
+     * Additional calls to {@link Query::where()} on the same column should merge values.
+     */
+    public function testWhereMerge() {
+        $qry = new Query();
+
+        $qry->addWhere('foo', 'bar')
+            ->addWhere('foo', [Db::OP_NE => 'baz'])
+            ->addWhere('bar', [Db::OP_NE => 'bam'])
+            ->addWhere('bar', 'qux');
+
+        $this->assertEquals(
+            [
+                'foo' => [Db::OP_EQ => 'bar', Db::OP_NE => 'baz'],
+                'bar' => [Db::OP_NE => 'bam', Db::OP_EQ => 'qux']
+            ],
+            $qry->toArray()['where']
+        );
+    }
+
+    /**
+     * Numeric arrays get converted to explicit IN statement when merging WHERE clauses.
+     */
+    public function testWhereMergeSimpleIn() {
+        $qry = new Query();
+
+        $qry->addWhere('foo', [1, 2])
+            ->addWhere('foo', 'bar')
+            ->addWhere('bar', 'baz')
+            ->addWhere('bar', [3, 4]);
+
+        $this->assertEquals(
+            [
+                'foo' => [Db::OP_IN => [1, 2], Db::OP_EQ => 'bar'],
+                'bar' => [Db::OP_EQ => 'baz', Db::OP_IN => [3, 4]]
+            ],
+            $qry->toArray()['where']
+        );
+    }
+}
